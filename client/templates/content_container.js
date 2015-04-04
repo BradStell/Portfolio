f76ad5a9d5d1330f52b45cea72d9aa179d5eca88
Brad Stell <bradstell87@gmail.com>
2015-04-03 20:28:55 -0400

    Done with dynamic loading of item templates

--- client/templates/content_container.js
+++ client/templates/content_container.js
@@ -1,5 +1,53 @@
 Template.paneContainer.helpers({
+    /*
     items: function() {
         return Items.find();
     }
+    */
+    items: [
+        {
+            id: 'item1',
+            value: 1
+        },
+        {
+            id: 'item2',
+            value: 2
+        },
+        {
+            id: 'item3',
+            value: 3
+        },
+        {
+            id: 'item4',
+            value: 4
+        },
+        {
+            id: 'item5',
+            value: 5
+        },
+        {
+            id: 'item6',
+            value: 6
+        },
+        {
+            id: 'item7',
+            value: 7
+        },
+        {
+            id: 'item8',
+            value: 8
+        },
+        {
+            id: 'item9',
+            value: 9
+        },
+        {
+            id: 'item10',
+            value: 10
+        },
+        {
+            id: 'item11',
+            value: 11
+        }
+    ]
 });
